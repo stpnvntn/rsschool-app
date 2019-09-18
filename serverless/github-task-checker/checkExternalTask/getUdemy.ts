51540bf74dea2a224bdf486a9bdf42bc8b7a4cfc
Dzmitry Tsebruk <mikhama@tut.by>
2019-09-18 13:19:00 +0300

    fix: update Udemy task scoring

--- serverless/github-task-checker/checkExternalTask/getUdemy.ts
+++ serverless/github-task-checker/checkExternalTask/getUdemy.ts
@@ -4,17 +4,14 @@ const COURSES_URLS = [
   '/course/foundations-of-front-end-development/',
   '/course/web-development-learn-by-doing-html5-css3-from-scratch-introductory/',
   '/course/master-the-basics-of-html5-css3-beginner-web-development/',
-]
+];
 
-const REQUIRED_COURSES_COUNT = 2;
+const REQUIRED_COURSES_COUNT = 1;
 
 export default async (certificates) => {
   try {
-    const [certId1, certId2] = certificates;
-    const isNotEmpty = Boolean(certId1 && certId2);
-    const isNotEqual = certId1 !== certId2;
-
     const urls = certificates
+      .filter(Boolean)
       .map(certId => `https://www.udemy.com/api-2.0/certificates/${certId}?fields[certificate]=code,completion_date,user,course&fields[user]=title&fields[course]=id,title,url`);
 
     const courses = (await Promise.all(urls.map(url => axios.get(url))))
@@ -30,13 +27,9 @@ export default async (certificates) => {
       })
       .filter(({ url }) => COURSES_URLS.includes(url));
 
-    const isTheSameUser = courses.slice(1)
-      .filter(({ username }) => courses[0].username === username)
-      .length > 0;
-
     console.log('Udemy courses =>', JSON.stringify(courses));
 
-    return isNotEmpty&& isNotEqual && isTheSameUser && courses.length === REQUIRED_COURSES_COUNT;
+    return courses.length >= REQUIRED_COURSES_COUNT;
   } catch (error) {
     console.log(`Error fetching ${certificates} certificates!\n${error.message}`);
     return false;
